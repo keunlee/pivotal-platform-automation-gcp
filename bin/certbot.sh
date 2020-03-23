85ddeee28722c17ed4143ac0c3a79dc846704416
Keun Lee <keun.lee@kinandcarta.com>
2020-03-23 20:02:59 +0000

    update certbot script

--- bin/certbot.sh
+++ bin/certbot.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-DOMAIN=${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME} 
-EMAIL=${MY_EMAIL}
 
 sudo certbot --agree-tos -m ${EMAIL} \
   certonly --cert-name  ${DOMAIN}  \
   --dns-google \
   -n \
-  -d "${DOMAIN}, *.${DOMAIN}, *.apps.${DOMAIN}, *.sys.${DOMAIN}, *.ws.${DOMAIN}, *.login.sys.${DOMAIN}, *.uaa.sys.${DOMAIN}, *.pks.${DOMAIN}, *.pks.pks.${DOMAIN}, *.concourse.${DOMAIN}" \
+  -d "${DOMAIN}, *.${DOMAIN}, *.apps.${DOMAIN}, *.sys.${DOMAIN}, *.ws.${DOMAIN}, *.login.sys.${DOMAIN}, *.uaa.sys.${DOMAIN}, *.pks.${DOMAIN},  *.concourse.${DOMAIN}" \
   --dns-google-credentials /home/keunlee/gcp_credentials.json \
   --dns-google-propagation-seconds 60
 
