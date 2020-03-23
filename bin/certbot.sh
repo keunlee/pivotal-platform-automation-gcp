1ad5eaab9fe15f2b7a60d23ef08aa19d638259c5
klee <klee@abc123.com>
2020-03-23 14:54:24 -0500

    update certbot script

--- bin/certbot.sh
+++ bin/certbot.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+DOMAIN=${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME} 
+EMAIL=${MY_EMAIL}
+
 sudo certbot --agree-tos -m ${EMAIL} \
   certonly --cert-name  ${DOMAIN}  \
   --dns-google \
   -n \
-  -d "${DOMAIN}, *.${DOMAIN}, *.apps.${DOMAIN}, *.sys.${DOMAIN}, *.ws.${DOMAIN}, *.login.sys.${DOMAIN}, *.uaa.sys.${DOMAIN}, *.pks.${DOMAIN}" \
+  -d "${DOMAIN}, *.${DOMAIN}, *.apps.${DOMAIN}, *.sys.${DOMAIN}, *.ws.${DOMAIN}, *.login.sys.${DOMAIN}, *.uaa.sys.${DOMAIN}, *.pks.${DOMAIN}, *.pks.pks.${DOMAIN}" \
   --dns-google-credentials /home/keunlee/gcp_credentials.json \
   --dns-google-propagation-seconds 60
 
