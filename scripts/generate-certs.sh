c2f133f284905ed7ca9ddfc6ad30b8f85418f28e
klee <klee@abc123.com>
2020-03-25 14:59:08 -0500

    update generate cert script

--- scripts/generate-certs.sh
+++ scripts/generate-certs.sh
@@ -1,1 +1,1 @@
-DOMAIN=${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME} EMAIL=${MY_EMAIL} ~/ops-manager-automation-cc/bin/certbot.sh
+DOMAIN=${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME} EMAIL=${MY_EMAIL} ../bin/certbot.sh
